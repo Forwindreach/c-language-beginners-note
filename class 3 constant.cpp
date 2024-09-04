3df70effb08e8c18c2a3ad6bed34f37a7e39e518
Forwindreach <vince2363743898@163.com>
2024-09-04 23:11:21 +0800

    class 3 constant

---
+++ class 3 constant.cpp
@@ -0,0 +1,42 @@
+//常量
+#include<stdio.h>
+
+//int main()
+//{
+
+
+//{//  1字面常量
+//	3.1415926;
+//	10
+//    "a"
+//	"abcde"
+// 
+// 
+// 
+//  2const修饰的常变量
+//const int num = 10;//num是变量，但是前缀const使其具有常量性，但其归根仍然是变量——常变量
+// 
+//  3.#define定义的标识符常量
+//#define min 20000
+//	int a = min;
+//	printf("%d\n", a);
+
+//  4.枚举常量——可以一一列举的常量//默认从零开始，只可以赋初始值
+	//使用enum定义//注意识别符，
+
+//enum sex
+//{
+//  MAN = 5,
+//  WOMAN,
+//  WATER,
+//};
+//int main()
+//{
+//	enum sex s = MAN;//定义一个变量s为MAN
+//	printf("%d\n",MAN);
+//	printf("%d\n",WOMAN);
+//	printf("%d\n",WATER);
+//
+//	return 0;
+//
+//}
