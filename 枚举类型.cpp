f4ed589661afd2289b93c9602811516491be4355
Forwindreach <vince2363743898@163.com>
2024-10-31 22:17:03 +0800

    Add files via upload

---
+++ 枚举类型.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+enum animal {Dog,Cat,Monkey,Invalid};//声明枚举类型 
+
+void dog(void)//定义无形参的函数 
+{
+	puts("汪汪");
+}
+void cat(void)
+{
+	puts("喵"); 
+}
+void monkey(void)
+{
+	puts("鸡鸡"); 
+}
+enum animal select(void)//定义返回类型为enum animal类型的函数 
+{
+	int tmp;
+	do{
+		printf("0——狗 1——猫 2——猴 3——结束");
+		scanf("%d",&tmp);
+		 
+	}while(tmp<Dog||tmp>Invalid);
+	return (enum animal)tmp;//将tmp的类型由int转为enum animal; 
+}
+int main(void)
+{
+	enum animal selected;//定义一个枚举变量 
+	do{
+		switch (selected=select()){//存入select函数运算出的枚举常量； 
+			case Dog :dog(); break;
+			case Cat :cat(); break;
+			case Monkey: monkey(); break;
+		}
+	}while(selected!=Invalid);//用户不选择结束则一直继续 
+	return 0;
+}
+ 
